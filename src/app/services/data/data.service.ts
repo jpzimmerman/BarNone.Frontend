a5b2cf5cf2ed5782622051ff881bd7aaa74d025b
Joshua Zimmerman <jpz6926@gmail.com>
2025-01-11 23:45:28 -0500

    adding backend API url to environment configuration, removing url as hard-coded valu in data service

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,59 +1,72 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { GuestOrder } from 'src/app/models/guestorder.model';
 import { Product } from 'src/app/models/product.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class DataService {
   requestOptions = {
     headers: new Headers({
       'Content-Type': 'application/json',
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Access-Control-Allow-Headers': 'Content-Type',
     }),
   };
 
   httpHeaders: HttpHeaders = new HttpHeaders({
-    'Content-Type': 'application/json'
-});
+    'Content-Type': 'application/json',
+  });
 
-  constructor(private http: HttpClient) {
-   }
+  constructor(private http: HttpClient) {}
 
-  async getMenuItems() : Promise<Product[]> {
+  async getMenuItems(): Promise<Product[]> {
     const result2: Product[] = [];
-    this.http.get<Product[]>("https://localhost:44375/api/menu/getmenuitems/").pipe(catchError(this.reportError))
-    .subscribe(data => {
-      data.forEach(x => {
-        result2.push(x)
-      })
-    });
+    this.http
+      .get<Product[]>(`${environment.baseBackendUrl}/api/menu/getmenuitems/`)
+      .pipe(catchError(this.reportError))
+      .subscribe((data) => {
+        data.forEach((x) => {
+          result2.push(x);
+        });
+      });
     return result2;
   }
 
-  getTags() : string[] {
+  getTags(): string[] {
     const result2: string[] = [];
-    this.http.get<string[]>("https://localhost:44375/api/menu/gettags")
-      .subscribe(data => {
-        data.forEach(x => {
-          result2.push(x)
-        })
-    });
+    this.http
+      .get<string[]>(`${environment.baseBackendUrl}/api/menu/gettags`)
+      .subscribe((data) => {
+        data.forEach((x) => {
+          result2.push(x);
+        });
+      });
     return result2;
   }
 
   addGuestOrder = (order: GuestOrder) => {
-    console.log(JSON.stringify(order))
-    this.http.put("https://localhost:44375/api/order/AddOrder", JSON.stringify(order), {headers: this.httpHeaders} )
-    .subscribe()
-  }
+    console.log(JSON.stringify(order));
+    this.http
+      .put(
+        'https://localhost:44375/api/order/AddOrder',
+        JSON.stringify(order),
+        { headers: this.httpHeaders }
+      )
+      .subscribe();
+  };
 
   reportError(error: HttpErrorResponse, caught: Observable<Product[]>) {
-    console.log(error.message)
-    return throwError(() => new Error(`${error.message}, ${JSON.stringify(caught)}`))
+    console.log(error.message);
+    return throwError(
+      () => new Error(`${error.message}, ${JSON.stringify(caught)}`)
+    );
   }
 }
