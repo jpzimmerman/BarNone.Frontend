2c07e1a5833d277e0e990d507d98680150bec19f
Joshua Zimmerman <jpz6926@gmail.com>
2025-03-03 01:41:57 -0500

    UI responsiveness update: added rudimentary hamburger menu to improve website responsiveness on mobile resolutions. Further styling to come.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppRoutingModule } from '../app-routing.module';
 
 @Component({
@@ -7,4 +7,19 @@ import { AppRoutingModule } from '../app-routing.module';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  hamburger = document.getElementById('hamburger');
+  navUL = document.getElementById('nav-ul');
+
+  async ngOnInit() {
+    this.navUL = document.getElementById('nav-ul');
+
+    this.hamburger?.addEventListener('click', () => {
+      this.navUL?.classList.toggle('show');
+    });
+  }
+
+  onHamburgerClick = () => {
+    this.navUL?.classList.toggle('show');
+  };
+}
