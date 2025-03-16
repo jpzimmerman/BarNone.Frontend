9a037beb95fe94b1fa3b91ed078feb273d05a80b
Joshua Zimmerman <jpz6926@gmail.com>
2025-03-16 16:48:23 -0400

    Bug fix: fixes quantity widget within 'add item' menu so that quantity updates on open (if quantity was updated with a value when the add item modal opened, then quantity widget will be populated on open)

--- src/app/bounties/bountypage/bountypage.component.ts
+++ src/app/bounties/bountypage/bountypage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Cocktail } from 'src/app/models/cocktail.model';
 import { DataService } from 'src/app/services/data/data.service';
 
@@ -9,8 +9,23 @@ import { DataService } from 'src/app/services/data/data.service';
   templateUrl: './bountypage.component.html',
   styleUrl: './bountypage.component.scss',
 })
-export class BountypageComponent {
+export class BountypageComponent implements OnInit, AfterViewInit {
+  allItems: Cocktail[] = [];
   bounties: Cocktail[] = [];
+  bountyHeader: string = '';
+  bountyDescription: string = '';
 
   constructor(public dataService: DataService) {}
+
+  async ngOnInit() {
+    this.allItems = await this.dataService.getMenuItems();
+  }
+
+  async ngAfterViewInit(): Promise<void> {
+    setTimeout(() => {
+      this.bounties = this.allItems.filter((x) => x.category == 'Shots');
+      this.bountyHeader = this.bounties[0].name;
+      this.bountyDescription = this.bounties[0].description;
+    }, 100);
+  }
 }
