68dcddf34cc042aa33ad4e72b31fe7ede854d5f1
Joshua Zimmerman <jpz6926@gmail.com>
2025-03-16 23:53:53 -0400

    Feature addition: basic Roll for Drink page put in place

--- src/app/rollfordrink/rollfordrink.component.ts
+++ src/app/rollfordrink/rollfordrink.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-rollfordrink',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './rollfordrink.component.html',
-  styleUrl: './rollfordrink.component.scss'
+  styleUrl: './rollfordrink.component.scss',
 })
-export class RollfordrinkComponent {
+export class RollfordrinkComponent implements OnInit {
+  async ngOnInit(): Promise<void> {
+    document.documentElement.style.setProperty('--startAngleX', '0deg');
+    document.documentElement.style.setProperty('--startAngleY', '160deg');
+    document.documentElement.style.setProperty('--startAngleZ', '40deg');
+  }
 
+  onRoll() {
+    var diceRollingSound = new Audio('../../assets/sound/dice-rolling.mp3');
+    diceRollingSound.play();
+  }
 }
