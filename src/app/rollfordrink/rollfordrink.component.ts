22dcd454e5bd7e4898e4ee66b70dded62eeeb3cd
Joshua Zimmerman <jpz6926@gmail.com>
2025-03-17 01:24:00 -0400

    UI enhancement: adjust menu display to remove shots menu (this will be moved to 'bounty board' page), and adjusts roll for drink page to add random initial orientation to d20 die

--- src/app/rollfordrink/rollfordrink.component.ts
+++ src/app/rollfordrink/rollfordrink.component.ts
@@ -8,13 +8,44 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './rollfordrink.component.scss',
 })
 export class RollfordrinkComponent implements OnInit {
+  constructor() {
+    document.documentElement.style.setProperty(
+      '--startAngleX',
+      `${Math.random() * 360}` + 'deg'
+    );
+    document.documentElement.style.setProperty(
+      '--startAngleY',
+      `-${Math.random() * 360}` + 'deg'
+    );
+    document.documentElement.style.setProperty(
+      '--startAngleZ',
+      `${Math.random() * 360}` + 'deg'
+    );
+  }
+
   async ngOnInit(): Promise<void> {
-    document.documentElement.style.setProperty('--startAngleX', '0deg');
-    document.documentElement.style.setProperty('--startAngleY', '160deg');
-    document.documentElement.style.setProperty('--startAngleZ', '40deg');
+    document.documentElement.style.setProperty(
+      '--startAngleX',
+      `${Math.random() * 360}` + 'deg'
+    );
+    document.documentElement.style.setProperty(
+      '--startAngleY',
+      `-${Math.random() * 360}` + 'deg'
+    );
+    document.documentElement.style.setProperty(
+      '--startAngleZ',
+      `${Math.random() * 360}` + 'deg'
+    );
   }
 
   onRoll() {
+    var dieElement = document.getElementById('d20-die');
+    dieElement?.classList.add('rolling');
+
+    setTimeout(() => {
+      dieElement?.classList.remove('rolling');
+    }, 5000);
+
     var diceRollingSound = new Audio('../../assets/sound/dice-rolling.mp3');
     diceRollingSound.play();
   }
