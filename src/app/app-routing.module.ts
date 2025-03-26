2c59469f9a32f1fba42297973a02aa7903bdfc07
Joshua Zimmerman <jpz6926@gmail.com>
2025-03-25 23:59:39 -0400

    wired up one-shot page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomePageComponent } from './homepage/home-page/home-page.component';
 import { MenupageComponent } from './menupage/menupage.component';
 import { BountypageComponent } from './bounties/bountypage.component';
 import { RollfordrinkComponent } from './rollfordrink/rollfordrink.component';
+import { OneshotComponent } from './oneshot/oneshot.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'menu', component: MenupageComponent },
   { path: 'bounties', component: BountypageComponent },
+  { path: 'oneshot', component: OneshotComponent },
   { path: 'rollfordrink', component: RollfordrinkComponent },
 ];
 
