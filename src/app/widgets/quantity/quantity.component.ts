9618a6142916c2e4d6a983c5ebb358e97ad0e4e5
Joshua Zimmerman <jpz6926@gmail.com>
2025-05-14 20:34:30 -0400

    Linting and auditing: fixing Node vulnerabilities, addressing issues found by ng lint

--- src/app/widgets/quantity/quantity.component.ts
+++ src/app/widgets/quantity/quantity.component.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { clamp } from 'lodash';
+import { clamp } from 'lodash-es';
 
 @Component({
   selector: 'app-quantity',
